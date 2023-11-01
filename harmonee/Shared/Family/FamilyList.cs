9d083085a7e16a4e249cec457ba9d486bacddff8
Ryan <ryan@ryannd.com>
2023-11-01 09:35:36 -0500

    Added FamilyList class

---
+++ harmonee/Shared/Family/FamilyList.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace harmonee.Shared.Family
+{
+    public class FamilyList
+    {
+        Guid Id;
+        string Name;
+        string Description;
+        string[] Items;
+    }
+}
