d852bc0e45c79ab2802c354ede3e34ba8a17846d
Ryan <ryan@ryannd.com>
2023-11-01 09:34:21 -0500

    Added FamilyEvent class

---
+++ harmonee/Shared/Family/FamilyEvent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace harmonee.Shared.Family
+{
+    public class FamilyEvent
+    {
+        Guid Id;
+        string Description;
+        DateTime StartTime;
+        DateTime EndTime;
+        Guid CreatedBy;
+    }
+}
