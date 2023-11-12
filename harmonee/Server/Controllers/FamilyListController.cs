86c4722f521aedff53c51ca79b1747ac4256f511
Ryan <ryan@ryannd.com>
2023-11-12 12:48:25 -0600

    Added family member, event, and list controllers

---
+++ harmonee/Server/Controllers/FamilyListController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace harmonee.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FamilyListController : ControllerBase
+    {
+    }
+}
