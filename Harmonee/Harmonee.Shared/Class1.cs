af27547e1716a7ecb4b94ad79b86c1e227bdafe3
Ryan <ryan@ryannd.com>
2024-03-01 20:11:57 -0600

    Init

---
+++ Harmonee/Harmonee.Shared/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace Harmonee.Shared
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ Harmonee/Harmonee.UI/Harmonee.UI/Program.cs
@@ -0,0 +1,35 @@
+using Harmonee.UI.Client.Pages;
+using Harmonee.UI.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(Harmonee.UI.Client._Imports).Assembly);
+
+app.Run();
