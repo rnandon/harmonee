a7d09e3dc8816ea59f325213a4301ca846f99c21
Ryan <ryan@ryannd.com>
2024-03-14 19:59:23 -0500

    Added BlazorServerUI, implemented basic auth with Auth0

---
+++ Harmonee/Harmonee.BlazorServerUI/Pages/Logout.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using Auth0.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Harmonee.BlazorServerUI.Pages;
+
+public class LogoutModel : PageModel
+{
+    public async Task OnGet(string redirectUri)
+    {
+        var authenticationProperties = new LogoutAuthenticationPropertiesBuilder()
+            .WithRedirectUri("/")
+            .Build();
+
+        await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+    }
+}
